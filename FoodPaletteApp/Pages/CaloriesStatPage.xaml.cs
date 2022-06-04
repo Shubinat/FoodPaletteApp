56e7de8a1d314824ec43caf091b8998598d36a86
Shubinat <shubinat729@gmail.com>
2022-06-05 00:32:02 +0300

    аналитика калорий

--- FoodPaletteApp/Pages/CaloriesStatPage.xaml.cs
+++ FoodPaletteApp/Pages/CaloriesStatPage.xaml.cs
@@ -23,6 +23,76 @@ namespace FoodPaletteApp.Pages
         public CaloriesStatPage()
         {
             InitializeComponent();
+
+        }
+
+        private void GenerateStat()
+        {
+            var list = App.Context.MealPerDay.ToList().OrderBy(x => x.DateTime).ToList();
+            CaloriesStat.Series.Clear();
+            CaloriesStat.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            if (ChBoxCalories.IsChecked == true)
+            {
+                var currSeries = CaloriesStat.Series.Add("Калории");
+                currSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+                currSeries.Points.DataBindXY(list.Select(x => x.DateTime).ToArray(), list.Select(x => x.Dish.Calories).ToArray());
+            }
+
+            if (ChBoxCarbohydrates.IsChecked == true)
+            {
+                var currSeries = CaloriesStat.Series.Add("Углеводы");
+                currSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+                currSeries.Points.DataBindXY(list.Select(x => x.DateTime).ToArray(), list.Select(x => x.Dish.Carbohydrates).ToArray());
+            }
+
+            if (ChBoxFats.IsChecked == true)
+            {
+                var currSeries = CaloriesStat.Series.Add("Жиры");
+                currSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+                currSeries.Points.DataBindXY(list.Select(x => x.DateTime).ToArray(), list.Select(x => x.Dish.Fats).ToArray());
+            }
+
+            if (ChBoxProteins.IsChecked == true)
+            {
+                var currSeries = CaloriesStat.Series.Add("Белки");
+                currSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+                currSeries.Points.DataBindXY(list.Select(x => x.DateTime).ToArray(), list.Select(x => x.Dish.Proteins).ToArray());
+            }
+
+        }
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog dialog = new PrintDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                dialog.PrintVisual(WinFormsHost, "Statistics");
+            }
+        }
+
+        private void ChBoxCalories_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateStat();
+        }
+
+        private void ChBoxProteins_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateStat();
+        }
+
+        private void ChBoxFats_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateStat();
+        }
+
+        private void ChBoxCarbohydrates_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateStat();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerateStat();
         }
     }
 }
